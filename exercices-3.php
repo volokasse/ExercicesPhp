73353201efbe84dae016b3073183a250b702971f
Volokasse <5593182+volokasse@users.noreply.github.com>
2020-02-25 14:46:13 +0100

    Exercice 3
    Signed-off-by: Volokasse <5593182+volokasse@users.noreply.github.com>

---
+++ exercices-3.php
@@ -0,0 +1,21 @@
+<?php
+
+/* EXERCICE NUMERO 3
+
+Ce script sera à exécuter via un terminal.
+grosso modo, avec un terminal ouvert dans le dossier actuel ça donnera :
+php exercices-3.php
+
+Je veux donc pouvoir appeler ce script avec les arguments suivants :
+- Coucou
+- Hello
+
+Et qu'en fonction de l'argument que je lui aurais passé il me donne une réponse différente :
+- Coucou -> Salut bro' !
+- Hello -> Whassup dude ?!
+
+Dans le cas d'un argument qui n'est pas l'un des deux, je veux que le script réponde :
+- Euh... ok... j'suis pressé, j'ai aqua poney, à la prochaine.
+
+=> voir @ http://zetcode.com/lang/php/basics/
+=> voir @ https://www.php.net/manual/fr/language.control-structures.php
